7eb7d43b43cdf1e04ebd1e3dd13f8325135f9402
Purushottam <kumarpuru11111@gmail.com>
2026-05-20 20:10:06 +0530

    Student file added

--- Main.java
+++ Main.java
@@ -6,5 +6,7 @@ public class Main {
         System.out.println("Output from java program");
         System.out.println("Hello");
         System.out.println("Testing the jenkins pipeline");
+        Student student=new Student();
+        student.sayHello();
     }
 }
---
+++ Student.java
@@ -0,0 +1,12 @@
+import java.sql.SQLOutput;
+
+public class Student {
+    public Student()
+    {
+        System.out.println("Creating a new student");
+    }
+    public void sayHello()
+    {
+        System.out.println("Hi , i am a student ");
+    }
+}
