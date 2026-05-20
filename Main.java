8be312c02ea299d1d93a829932a79500ba527a2e
Purushottam <kumarpuru11111@gmail.com>
2026-05-20 19:40:11 +0530

    Initial commit

---
+++ Main.java
@@ -0,0 +1,9 @@
+public class Main {
+
+    public static void main(String[] args)
+    {
+        System.out.println("This is a java program");
+        System.out.println("Output from java program");
+        System.out.println("Hello");
+    }
+}
